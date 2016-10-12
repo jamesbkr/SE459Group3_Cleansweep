62cbfe02b794f7411d7909600c41cc7bc03f2b80
James Becker <jbecker@localhost.localdomain>
2016-10-11 21:01:57 -0400

    CS-25 CS-26 CS-27 expanded room and vacuum

---
+++ CleanSweep/src/main/java/room/Area.java
@@ -0,0 +1,6 @@
+package room;
+
+public interface Area {
+	public void updateLocation(ThingsInRoom x, int y, int z);
+	public void addLocation(ThingsInRoom x, int y, int z);
+}
--- CleanSweep/src/main/java/vacuum/Vacuum.java
+++ CleanSweep/src/main/java/vacuum/Vacuum.java
@@ -1,12 +1,22 @@
 package vacuum;
 
 import room.Room;
+import room.ThingsInRoom;
 
 public class Vacuum {
 	int storage = 50;
 	int batteryLife = 100;
 	String name;
 	Room room;
+	int[] currentLocation = {0,0};
+	
+	ThingsInRoom front = ThingsInRoom.NOTHING;
+	ThingsInRoom back = ThingsInRoom.NOTHING;
+	ThingsInRoom left = ThingsInRoom.NOTHING;
+	ThingsInRoom right = ThingsInRoom.NOTHING;
+	
+	ThingsInRoom center = ThingsInRoom.BASE;
+	
 	
 	
 	public Vacuum(String name, Room room){
@@ -14,14 +24,45 @@ public class Vacuum {
 		this.room = room;
 	}
 	
+	public void run(){
+		this.move();
+	}
 	
-	public void checkStorage(){
-		if (this.storage+1 == 51){
-			returnToBase();
-		}
-		
+	
+	
+	private int checkStorage(){
+		return this.storage;
 	}
 
+	private void move(){
+		do{
+			//read left sensor
+			//read right sensor
+			//read front sensor
+			//read back sensor
+			//assign left sensor
+			//assign right sensor
+			//assign front sensor
+			//assign back sensor
+			
+			if(front != ThingsInRoom.OBSTACLE){
+				center = front; 
+				
+				
+				
+				
+				
+				
+			}
+			
+		}while((checkStorage() < 50));
+		
+		returnToBase();
+		
+		}
+		
+		
+	
 
 	private void returnToBase() {
 		// TODO Auto-generated method stub
