95b4f250e3ab903fddf3a308f4f1557131c4c5eb
achanta0216 <achanta0216@gmail.com>
2016-10-31 06:26:24 -0500

    output to text file

--- CleanSweep/src/main/java/diagnostics/StorageCheck.java
+++ CleanSweep/src/main/java/diagnostics/StorageCheck.java
@@ -1,36 +1,42 @@
 package diagnostics;
 import vacuum.*;
-public class StorageCheck {
+
+public class PowerCheck {
+	
 	//Just a basic if statement prompt scanning the vacuum.
 	//Do we want this to be a simple check or a process that goes through every move of the vacuum?
-	//If so I think we can add a token here just for passing sake either an int or something for the all clear
-	// Also Activity Log framework, just ArrayList for now.
+	// Made it strings so we can add them to an ArrayList if possible for the Activity Log
+
 	
 	//Left over tokens from other idea
-	//Basically 0 for good to go, 1 for empty me, and 2 for complete error
-	public static String Diagnostic(Vacuum vacuumSource){
-		int storage = vacuumSource.checkStorage();
+	//Basically 0 for good to go, 1 for no charge left, and 2 for complete error
+	public static String PowerCheck(Vacuum vacuumSource){
+		int batterylife = vacuumSource.checkBatteryLife();
 		String activityHolder;
-		if (storage==50){
-			System.out.println( "Storage Capacity Left: "+ storage) ;
-			activityHolder= "Storage Capacity Left: "+ storage;
-		}else if (storage==0){
-			System.out.println( "Empty Me!" );
-			activityHolder= "Empty Me!" ;
-			//Simple prompt tie into halting
+		//Integer token = 0;
+		if (batterylife==100){
+			System.out.println( "Battery Fully Charged: "+ batterylife+"%") ;	
+			activityHolder= "Battery Fully Charged: "+ batterylife+"%";
+			//token= 0;
+		}else if (batterylife==0){
+			System.out.println( "Please Charge : Battery Empty: "+ batterylife+"%" );
+			activityHolder= "Please Charge : Battery Empty: "+ batterylife+"%" ;
 			//token= 1;
-		}else if (storage<0){
-			System.out.println( "Error storage at " + storage);
-			activityHolder= "Error storage at " + storage;
-			//Do we actually want to add an error throw?
-			//token = 2
+			//Create a recharge token? to prompt returning to recharge base
+		}else if (batterylife<0){
+			System.out.println( "Error Batterylife at " + batterylife);
+			activityHolder= "Error Batterylife at " + batterylife;
+			//token= 2;
+			// Do we actually want to add an error throw or anything of the like.
 		}else{
-			System.out.println("Capacity left: "+ storage);
-			activityHolder= "Capacity left: "+ storage;
-			//token = 0;
+			System.out.println("Battery at: "+ batterylife);
+			activityHolder="Battery at: "+ batterylife;
 		}
+		
+		
+		// Holding onto return statement if we need it to return a certificate or validation of some sort
 		//return token;
 		return activityHolder;
+		
 	}
-
 }
